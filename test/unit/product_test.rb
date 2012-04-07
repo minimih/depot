76b67f41bec5ec2f5de526697996176c2d190d25
minimih <minimih@gmail.com>
2012-04-07 16:20:23 +0200

    chapter 7 update Validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+  
   test "product attributes must not be empty" do
   	product = Product.new
 	assert product.invalid?
@@ -8,6 +10,16 @@ class ProductTest < ActiveSupport::TestCase
   	assert product.errors[:description].any?
   	assert product.errors[:price].any?
   	assert product.errors[:image_url].any?
+  end
+  
+  test "product is not valid without a unique title" do
+  	product = Product.new(title:	   products(:ruby).title,
+  						  description: "yyy",
+  						  price:	   1,
+  						  image_url:   "fred.gif")
+  	
+  	assert !product.save
+  	assert_equal "has already been taken", product.errors[:title].join('; ')
   end
   
   test "product price must be positive" do
